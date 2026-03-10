60a8fc68a120c0e7822b001d24ef131c89f260b9
JoelVCrasta <loejstarc@gmail.com>
2026-03-10 16:58:24 +0530

    updated telemetry to use redis for caching assets

--- backend/src/telemetry/controllers/telemetrySocket.ts
+++ backend/src/telemetry/controllers/telemetrySocket.ts
@@ -1,80 +1,104 @@
-import { prisma } from '../../db/prisma.js';
-import { Server, Socket } from 'socket.io';
+import { prisma } from "../../db/prisma.js";
+import { Server, Socket } from "socket.io";
+import redisClient from "../../lib/redis.js";
+import { ISystemTelemetryPayload } from "../types.js";
 
 export const setUpTelemetrySocket = (io: Server) => {
-    io.on('connection', (socket: Socket) => {
-        console.log(`New connection established: ${socket.id}`);
-        console.log(socket.data)
+  io.on("connection", (socket: Socket) => {
+    console.log(`New connection established: ${socket.id}`);
 
-        socket.on('telemetry_stream', async (payload) => {
-            try {
-                const { mac_address, cpu, memory, battery } = payload;
-                console.log(payload);
+    socket.on("telemetry_stream", async (payload: ISystemTelemetryPayload) => {
+      try {
+        const { mac_address, cpu, memory, battery, timestamp } = payload;
+        if (!mac_address) return;
 
-                if (!mac_address) return;
+        if (!socket.data.mac_address) {
+          socket.data.mac_address = mac_address;
+        }
 
-                if (!socket.data.mac_address) {
-                    socket.data.mac_address = mac_address;
-                }
+        const cacheKey = `mac_to_asset_id:${mac_address}`;
+        let assetId = await redisClient.get(cacheKey);
 
-                const asset = await prisma.asset.upsert({
-                    where: { macAddr: mac_address },
-                    update: { status: 'ONLINE' },
-                    create: {
-                        macAddr: mac_address,
-                        status: 'ONLINE',
-                        cpuName: cpu?.cpu_name
-                    }
-                });
+        if (!assetId) {
+          const asset = await prisma.asset.findUnique({
+            where: { macAddr: mac_address },
+            select: { id: true },
+          });
 
-                const telemetryRecord = await prisma.telemetry.create({
-                    data: {
-                        assetId: asset.id,
-                        cpuTotalUsagePercent: cpu?.total_usage_percent,
-                        cpuPerCoreUsage: cpu?.per_core_usage_percent || [],
-                        cpuFrequency: cpu?.frequency,
-                        cpuTemperature: cpu?.temperature,
-                        memoryTotal: memory?.total,
-                        memoryAvailable: memory?.available,
-                        memoryUsed: memory?.used,
-                        memoryUsagePercent: memory?.usage_percent,
-                        batteryPercent: battery?.percent,
-                        batteryPowerPlugged: battery?.power_plugged,
-                        batterySecondsLeft: battery?.seconds_left,
-                    }
-                });
+          if (!asset) {
+            console.warn(`Unauthorized device: ${mac_address}`);
+            return;
+          }
 
-                io.emit('dashboard_update', {
-                    asset,
-                    latestTelemetry: telemetryRecord
-                });
-            } catch (error) {
-                console.error('Error processing telemetry stream:', error);
-            }
-        });
+          assetId = asset.id as string;
+          await redisClient.set(cacheKey, assetId, {
+            expiration: {
+              type: "EX",
+              value: 3600,
+            },
+          });
+        }
 
-        socket.on('disconnect', async () => {
-            console.log(`Client disconneccted: ${socket.id}`);
+        const [_updatedAsset, telemetryRecord] = await prisma.$transaction([
+          prisma.asset.update({
+            where: { id: assetId },
+            data: { status: "ONLINE" },
+          }),
+          prisma.telemetry.create({
+            data: {
+              assetId: assetId,
+              cpuName: cpu.cpu_name,
+              cpuTotalUsagePercent: cpu.total_usage_percent,
+              cpuPerCoreUsage: cpu.per_core_usage_percent,
+              cpuFrequency: cpu.frequency,
+              cpuTemperature: cpu.temperature,
+              memoryTotal: memory.total,
+              memoryAvailable: memory.available,
+              memoryUsed: memory.used,
+              memoryUsagePercent: memory.usage_percent,
+              batteryPercent: battery.percent,
+              batteryPowerPlugged: battery.power_plugged,
+              batterySecondsLeft: battery.seconds_left,
+              timestamp: new Date(timestamp * 1000),
+            },
+          }),
+        ]);
 
-            const disconnectedMac = socket.data.mac_address;
-            console.log(disconnectedMac);
-            if (disconnectedMac) {
-                try {
-                    await prisma.asset.update({
-                        where: { macAddr: disconnectedMac },
-                        data: { status: 'OFFLINE' }
-                    });
-
-                    console.log(`Asset ${disconnectedMac} marked as offline`);
-
-                    io.emit('asset_status_change', {
-                        macAddr: disconnectedMac,
-                        status: 'OFFLINE'
-                    });
-                } catch (error) {
-                    console.error(`Failed to update offline status for ${disconnectedMac}`);
-                }
-            }
-        });
+        // TODO: add rooms to send to a particular user
+        /*         io.emit("dashboard_update", {
+          assetId: telemetryRecord.assetId,
+          mac_address,
+          latestTelemetry: telemetryRecord,
+        }); */
+      } catch (error) {
+        console.error("Error processing telemetry stream:", error);
+      }
     });
+
+    socket.on("disconnect", async () => {
+      console.log(`Client disconnected: ${socket.id}`);
+
+      const disconnectedMac = socket.data.mac_address;
+      console.log(disconnectedMac);
+      if (disconnectedMac) {
+        try {
+          await prisma.asset.update({
+            where: { macAddr: disconnectedMac },
+            data: { status: "OFFLINE" },
+          });
+
+          console.log(`Asset ${disconnectedMac} marked as offline`);
+
+          io.emit("asset_status_change", {
+            macAddr: disconnectedMac,
+            status: "OFFLINE",
+          });
+        } catch (error) {
+          console.error(
+            `Failed to update offline status for ${disconnectedMac}`,
+          );
+        }
+      }
+    });
+  });
 };
