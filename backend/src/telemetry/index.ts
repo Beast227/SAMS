d25050d46f2a7a5d6b489aeda4d4f477beb49e5d
pavin <pavin227@gmail.com>
2026-03-11 10:34:54 +0530

    fixes: fixed import issues

--- backend/src/telemetry/index.ts
+++ backend/src/telemetry/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getHistoryTelemetry } from "./controllers/telemetryController.js";
+import { getHistoryTelemetry } from "./controller/telemetryController.js";
 import verifyUser from "../users/controller/userAuthMiddleware.js";
 
 
